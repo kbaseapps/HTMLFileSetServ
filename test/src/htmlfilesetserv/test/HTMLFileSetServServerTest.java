8275136527a959b46548e93f8d4d054847ce26dc
Gavin Price <gaprice@lbl.gov>
2016-10-17 16:46:32 -0700

    Basic happy path working

--- test/src/htmlfilesetserv/test/HTMLFileSetServServerTest.java
+++ test/src/htmlfilesetserv/test/HTMLFileSetServServerTest.java
@@ -1,14 +1,28 @@
 package htmlfilesetserv.test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.InputStream;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-import junit.framework.Assert;
-
+import org.apache.commons.io.IOUtils;
 import org.ini4j.Ini;
 import org.ini4j.Profile.Section;
 import org.junit.AfterClass;
@@ -20,42 +34,88 @@ import htmlfilesetserv.HTMLFileSetHTTPServer;
 import us.kbase.auth.AuthToken;
 import us.kbase.auth.AuthService;
 import us.kbase.common.service.JsonServerSyslog;
+import us.kbase.common.service.Tuple9;
+import us.kbase.common.service.UObject;
 import us.kbase.common.test.TestCommon;
+import us.kbase.common.test.TestException;
+import us.kbase.workspace.CreateWorkspaceParams;
+import us.kbase.workspace.ObjectSaveData;
+import us.kbase.workspace.SaveObjectsParams;
+import us.kbase.workspace.WorkspaceClient;
+import us.kbase.workspace.WorkspaceIdentity;
 
 public class HTMLFileSetServServerTest {
 	
-	private static AuthToken token = null;
-	private static Map<String, String> config = null;
+	private static AuthToken TOKEN1;
+	private static AuthToken TOKEN2;
+	private static Map<String, String> CONFIG = null;
 	
 	private static Path SCRATCH;
 	private static HTMLFileSetHTTPServer HTML_SERVER;
+	private static Tuple9<Long, String, String, String, Long, String, String,
+			String, Map<String, String>> WS_READ;
+	private static Tuple9<Long, String, String, String, Long, String, String,
+			String, Map<String, String>> WS_PRIV;
+	
+	private static WorkspaceClient WS1;
+	private static WorkspaceClient WS2;
+	
+	private static URL HTTP_ENDPOINT;
 	
 	@BeforeClass
 	public static void init() throws Exception {
-		//TODO TEST AUTH make configurable?
-		token = AuthService.validateToken(System.getenv("KB_AUTH_TOKEN"));
 		
 		String configFilePath = System.getenv("KB_DEPLOYMENT_CONFIG");
 		File deploy = new File(configFilePath);
 		Ini ini = new Ini(deploy);
-		config = ini.get("HTMLFileSetServ");
-		SCRATCH = Paths.get(config.get("scratch"));
+		CONFIG = ini.get("HTMLFileSetServ");
+		SCRATCH = Paths.get(CONFIG.get("scratch"));
 		Path testCfg = SCRATCH.resolve("../test.cfg");
 		Properties p = new Properties();
 		p.load(Files.newInputStream(testCfg));
-		System.out.println(p);
+
+		//TODO TEST AUTH make configurable?
+		TOKEN1 = AuthService.validateToken(System.getenv("KB_AUTH_TOKEN"));
+		final String t2 = p.getProperty("test_user2_token");
+		if (t2 == null || t2.trim().isEmpty()) {
+			throw new TestException(
+					"test property test_user2_token not supplied");
+		}
+		//TODO TEST AUTH make configurable?
+		TOKEN2 = AuthService.validateToken(t2);
+		if (TOKEN1.getUserName().equals(TOKEN2.getUserName())) {
+			throw new TestException(String.format(
+					"The two users specified in the test config " +
+					"must be different users: %s %s",
+					TOKEN1.getUserName(), TOKEN2.getUserName()));
+		}
 		
 		// These lines are necessary because we don't want to start linux
 		// syslog bridge service
 		JsonServerSyslog.setStaticUseSyslog(false);
 		JsonServerSyslog.setStaticMlogFile(
-				new File(config.get("scratch"), "test.log").getAbsolutePath());
+				new File(CONFIG.get("scratch"), "test.log").getAbsolutePath());
 		
-		final String wsURL = config.get("workspace-url");
-
+		final String wsURL = CONFIG.get("workspace-url");
+		WS1 = new WorkspaceClient(new URL(wsURL), TOKEN1);
+		WS2 = new WorkspaceClient(new URL(wsURL), TOKEN2);
+		
+		long suffix = System.currentTimeMillis();
+		final String wsName1 = "test_HTMLFileSetServ_" + suffix;
+		WS_READ = WS1.createWorkspace(new CreateWorkspaceParams()
+				.withWorkspace(wsName1));
+		WS_PRIV = WS2.createWorkspace(new CreateWorkspaceParams()
+				.withWorkspace(wsName1 + "private"));
 		HTML_SERVER = startupHTMLServer(wsURL);
 		int htmlport = HTML_SERVER.getServerPort();
+		HTTP_ENDPOINT = new URL("http://localhost:" + htmlport + "/api/v1/");
 		System.out.println("Started html server on port " + htmlport);
+		
+		//makes httpurlconnection handle cookies correctly
+		final CookieManager cookieManager = new CookieManager();
+		CookieHandler.setDefault(cookieManager);
+		
+		loadTestData();
 	}
 	
 	private static HTMLFileSetHTTPServer startupHTMLServer(
@@ -117,19 +177,57 @@ public class HTMLFileSetServServerTest {
 			HTML_SERVER.stopServer();
 			System.out.println("Done");
 		}
+		if (WS_READ != null) {
+			System.out.println("Removing readable test workspace");
+			WS1.deleteWorkspace(new WorkspaceIdentity()
+					.withId(WS_READ.getE1()));
+		}
+		if (WS_PRIV != null) {
+			System.out.println("Removing private test workspace");
+			WS2.deleteWorkspace(new WorkspaceIdentity()
+					.withId(WS_PRIV.getE1()));
+			
+		}
+	}
+	
+	public static void loadTestData() throws Exception {
+		final String contents = "file1";
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try (final ZipOutputStream zos = new ZipOutputStream(
+				baos, StandardCharsets.UTF_8);) {
+		
+			final ZipEntry ze = new ZipEntry("file.txt");
+			zos.putNextEntry(ze);
+			final byte[] b = contents.getBytes(StandardCharsets.UTF_8);
+			zos.write(b, 0, b.length);
+		}
+		System.out.println(baos.size());
+		final Map<String, Object> obj = new HashMap<>();
+		final String enc = Base64.getEncoder()
+				.encodeToString(baos.toByteArray());
+		obj.put("file", enc);
+		WS1.saveObjects(new SaveObjectsParams().withId(WS_READ.getE1())
+				.withObjects(Arrays.asList(new ObjectSaveData()
+						.withData(new UObject(obj))
+						.withName("html")
+						.withType("HTMLFileSetUtils.HTMLFileSet-0.1"))
+						)
+				);
 	}
 
 	@Test
-	public void testYourMethod() throws Exception {
-		// Prepare test objects in workspace if needed using 
-		// wsClient.saveObjects(new SaveObjectsParams().withWorkspace(getWsName()).withObjects(Arrays.asList(
-		//         new ObjectSaveData().withType("SomeModule.SomeType").withName(objName).withData(new UObject(obj)))));
-		//
-		// Run your method by
-		// YourRetType ret = impl.yourMethod(params, token);
-		//
-		// Check returned data with
-		// Assert.assertEquals(..., ret.getSomeProperty());
-		// ... or other JUnit methods.
+	public void testBasicHappyPath() throws Exception {
+		final URL u = new URL(HTTP_ENDPOINT.toString() + WS_READ.getE1() +
+				"/1/-/$/file.txt");
+		final HttpURLConnection hc = (HttpURLConnection) u.openConnection();
+		hc.setRequestProperty("Cookie", "token=" + TOKEN1.getToken());
+		hc.setDoInput(true);
+		final String contents;
+		try (final InputStream is = hc.getInputStream()) {
+			contents = IOUtils.toString(is);
+		}
+		
+		assertThat("incorrect file contents", contents, is("file1"));
+		assertThat("correct return code", hc.getResponseCode(), is(200));
 	}
 }
