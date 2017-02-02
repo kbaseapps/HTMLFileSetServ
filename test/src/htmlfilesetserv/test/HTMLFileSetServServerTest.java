63d81b92e918e09a0a6515bafa7126d1404cfaa7
Gavin Price <gaprice@lbl.gov>
2017-02-02 15:26:15 -0800

    Add some content type tests

--- test/src/htmlfilesetserv/test/HTMLFileSetServServerTest.java
+++ test/src/htmlfilesetserv/test/HTMLFileSetServServerTest.java
@@ -13,12 +13,15 @@ import java.io.UnsupportedEncodingException;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.HashMap;
@@ -32,6 +35,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.NullOutputStream;
 import org.ini4j.Ini;
 import org.ini4j.Profile.Section;
 import org.junit.AfterClass;
@@ -94,6 +98,9 @@ public class HTMLFileSetServServerTest {
 	
 	private static final Path PATH_TO_TEST_ZIP_WITH_DIRS = Paths.get(
 			"./test/src/htmlfilesetserv/test/zipWithDirectories.zip").toAbsolutePath();
+	
+	private static final Path PATH_TO_TEST_ZIP_CONTENT_TYPE = Paths.get(
+			"./test/src/htmlfilesetserv/test/contenttype.zip").toAbsolutePath();
 	
 	@BeforeClass
 	public static void init() throws Exception {
@@ -330,11 +337,19 @@ public class HTMLFileSetServServerTest {
 				"dirs.zip", "zip");
 		final String dirhandle = makeHandle(dirnode);
 		CREATED_NODES.put(dirnode.getId().getId(), new NodeAndHandle(dirnode, dirhandle));
+		
+		final byte[] zipContentType = Files.readAllBytes(PATH_TO_TEST_ZIP_CONTENT_TYPE);
+		final ShockNode contentTypeNode = bsc.addNode(new ByteArrayInputStream(zipContentType),
+				"contenttype.zip", "zip");
+		final String contentTypeHandle = makeHandle(contentTypeNode);
+		CREATED_NODES.put(contentTypeNode.getId().getId(),
+				new NodeAndHandle(contentTypeNode, contentTypeHandle));
 
 		
 		saveKBaseReport(WS1, WS_READ.getE1(), "good2",
 				Arrays.asList(node1, node2));
 		saveKBaseReport(WS1, WS_READ.getE1(), "dirs", Arrays.asList(dirnode));
+		saveKBaseReport(WS1, WS_READ.getE1(), "contenttype", Arrays.asList(contentTypeNode));
 		saveKBaseReport(WS1, WS_READ.getE1(), "shocknofile2",
 				Arrays.asList(node1, emptynode));
 		OBJ_TO_NODES.put("shocknofile2", Arrays.asList(node1, emptynode));
@@ -635,6 +650,38 @@ public class HTMLFileSetServServerTest {
 				"cookie");
 	}
 	
+	@Test
+	public void testSuccessContentTypeOctet() throws Exception {
+		final String path = "/" + WS_READ.getE2() + "/contenttype/-/$/0/foo";
+		final String absref = WS_READ.getE1() + "/13/1";
+		testSuccessContentType(path, absref, TOKEN1_MUNGED, "0/foo", "application/octet-stream",
+				"87a3bc393e60611fee6de78e75f342b6");
+	}
+	
+	@Test
+	public void testSuccessContentTypeCSS() throws Exception {
+		final String path = "/" + WS_READ.getE2() + "/contenttype/-/$/0/foo.css";
+		final String absref = WS_READ.getE1() + "/13/1";
+		testSuccessContentType(path, absref, TOKEN1_MUNGED, "0/foo.css", "text/css",
+				"0a57464fefc7bc57696691a4cd00df35");
+	}
+	
+	@Test
+	public void testSuccessContentTypeCSV() throws Exception {
+		final String path = "/" + WS_READ.getE2() + "/contenttype/-/$/0/foo.csv";
+		final String absref = WS_READ.getE1() + "/13/1";
+		testSuccessContentType(path, absref, TOKEN1_MUNGED, "0/foo.csv", "text/csv",
+				"951f25bcb4fb519b8b5eeb00285c2ea0");
+	}
+	
+	@Test
+	public void testSuccessContentTypeText() throws Exception {
+		final String path = "/" + WS_READ.getE2() + "/contenttype/-/$/0/foo.txt";
+		final String absref = WS_READ.getE1() + "/13/1";
+		testSuccessContentType(path, absref, TOKEN1_MUNGED, "0/foo.txt", "text/plain",
+				"d3b07384d113edec49eaa6238ad5ff00");
+	}
+
 	@Test
 	public void testFailNoRead() throws Exception {
 		final String path = "/" + WS_PRIV.getE1() + "/html/-/$/file.txt";
@@ -1102,6 +1149,30 @@ public class HTMLFileSetServServerTest {
 			final String authMode,
 			final String contentType)
 			throws Exception {
+		final HttpURLConnection hc = getConnectionAndCheckHeader(path, token, authMode,
+				contentType);
+		final String contents;
+		try (final InputStream is = hc.getInputStream()) {
+			contents = IOUtils.toString(is);
+		}
+		assertThat("incorrect file contents", contents, is(testcontents));
+
+		final Path filepath = SCRATCH.resolve("cache").resolve(absref).resolve(filename);
+		final String cacheContents;
+		try (final InputStream is = Files.newInputStream(filepath)) {
+			cacheContents = IOUtils.toString(is);
+		}
+		assertThat("incorrect cache file contents", cacheContents, is(testcontents));
+		
+		assertNoTempFilesLeftOnDisk();
+	}
+
+	private HttpURLConnection getConnectionAndCheckHeader(
+			final String path,
+			final String token,
+			final String authMode,
+			final String contentType)
+			throws MalformedURLException, IOException {
 		logStartTest();
 		final URL u = new URL(HTTP_ENDPOINT.toString() + path);
 		final HttpURLConnection hc = (HttpURLConnection) u.openConnection();
@@ -1118,22 +1189,46 @@ public class HTMLFileSetServServerTest {
 		}
 		final String ct = hc.getHeaderField("Content-Type");
 		assertThat("incorrect Content-Type", ct, is(contentType));
-		final String contents;
-		try (final InputStream is = hc.getInputStream()) {
-			contents = IOUtils.toString(is);
+		return hc;
+	}
+	
+	
+	private void testSuccessContentType(
+			final String path,
+			final String absref,
+			final String token,
+			final String filename,
+			final String contenttype,
+			final String md5)
+			throws Exception {
+		final HttpURLConnection hc = getConnectionAndCheckHeader(path, token, "cookie",
+				contenttype);
+		
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		DigestInputStream dis = new DigestInputStream(hc.getInputStream(), md);
+		IOUtils.copy(dis, new NullOutputStream()); // exhaust the stream
+		String digest = getMD5fromDigest(md);
+		assertThat("incorrect MD5", digest, is(md5));
+		
+		final Path filepath = SCRATCH.resolve("cache").resolve(absref).resolve(filename);
+		md = MessageDigest.getInstance("MD5");
+		dis = new DigestInputStream(hc.getInputStream(), md);
+		try (final InputStream is = new DigestInputStream(Files.newInputStream(filepath), md)) {
+			IOUtils.copy(is, new NullOutputStream());
 		}
-		assertThat("incorrect file contents", contents, is(testcontents));
-
-		final Path filepath = SCRATCH.resolve("cache").resolve(absref)
-				.resolve(filename);
-		final String cacheContents;
-		try (final InputStream is = Files.newInputStream(filepath)) {
-			cacheContents = IOUtils.toString(is);
-		}
-		assertThat("incorrect cache file contents", cacheContents,
-				is(testcontents));
+		digest = getMD5fromDigest(md);
+		assertThat("incorrect cache file md5", digest, is(md5));
 		
 		assertNoTempFilesLeftOnDisk();
+	}
+	
+	private String getMD5fromDigest(final MessageDigest digest) {
+		final byte[] d = digest.digest();
+		final StringBuilder sb = new StringBuilder();
+		for (final byte b : d) {
+			sb.append(String.format("%02x", b));
+		}
+		return sb.toString();
 	}
 
 	private void setUpAuthHeader(
@@ -1157,7 +1252,7 @@ public class HTMLFileSetServServerTest {
 		final Exception e = new Exception();
 		e.fillInStackTrace();
 		String method = null;
-		for (int i = 1; i < 5; i++) {
+		for (int i = 1; i < 6; i++) {
 			final String mn = e.getStackTrace()[i].getMethodName();
 			if (!mn.equals("testFail") && !mn.equals("testSuccess")) {
 				method = mn;
